99c1142b4f510d5153f85649e48a2f9bd314d74a
yuma-k <lipton.happy.milktea@gmail.com>
2021-09-20 19:27:47 +0900

    mongodbにツイートを収納するテスト

--- backend/src/app/mongo_test.py
+++ backend/src/app/mongo_test.py
@@ -1,12 +1,18 @@
 from pymongo import MongoClient
 import os
+from datetime import *
 
 # client = MongoClient('mongodb://db:27017/')
 db_host = os.environ['MONGO_DB_HOST']
 db_user = os.environ['MONGO_DB_USERNAME']
 db_pass = os.environ['MONGO_DB_PASSWORD']
+db_name = os.environ['MONGO_DB_DATABASE']
 with MongoClient(db_host, 27017, username=db_user, password=db_pass) as client:
-    test_db = client.tweet_db
-    test_collection = test_db.test_collection
+    tweet_db = client[db_name]
+    tweet_collection = tweet_db.tweets
 
-    test_collection.insert_one({"name": "test", "age": 24, "tag": ["test1", "test2"]})
+    tweet_collection.insert_one({
+        "id": "1438301756174356483",
+        "date": datetime.strptime('Thu Sep 16 00:40:00 +0000 2021', '%a %b %d %H:%M:%S %z %Y').astimezone(
+            timezone(timedelta(hours=+9))),
+        "hashtag": ["test1", "test2"]})
