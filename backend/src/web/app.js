af222c66792a003e82711133496f89fd02630fd8
yuma-k <lipton.happy.milktea@gmail.com>
2021-09-26 00:04:45 +0900

    ハッシュタグが無い時にも取得できるように変更

--- backend/src/web/app.js
+++ backend/src/web/app.js
@@ -18,24 +18,38 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 
 app.get('/tweet/', async (req, res) => {
-    const hashtags = req.query.hashtags
-    const since = Date.parse(req.query.since)
-    const until = Date.parse(req.query.until)
+    const hashtags = req.query.hashtags || []
+    const since = Date.parse(req.query.since) || new Date()
+    const until = Date.parse(req.query.until) || new Date()
 
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET')
 
-    const tweetData = await tweetSchema.find({
-        hashtag: {
-            $in: hashtags
-        },
-        date: {
-            $gte: since,
-            $lte: until
+    // hashtagsが無い時の分岐だけどもう少し賢い方法がありそう…
+    const query = (() => {
+        if (hashtags.length != 0) {
+            return {
+                hashtag: {
+                    $in: hashtags
+                },
+                date: {
+                    $gte: since,
+                    $lte: until
+                }
+            }
+        } else {
+            return {
+                date: {
+                    $gte: since,
+                    $lte: until
+                }
+            }
         }
-    })
+    })()
 
-    const resData = tweetData.map(tweet => ({str_id: tweet._id}))
+    const tweetData = await tweetSchema.find(query)
+
+    const resData = tweetData.map(tweet => ({ str_id: tweet._id }))
 
     res.json(resData)
 })
